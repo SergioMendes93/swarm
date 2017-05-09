fb7c200934d8f0465bbc5fc5c12ecf25dc55f278
SergioMendes93 <smendes_93@hotmail.com>
2017-05-09 22:57:35 +0100

    bug fix

--- scheduler/strategy/energy.go
+++ scheduler/strategy/energy.go
@@ -186,7 +186,7 @@ func (p *EnergyPlacementStrategy) RankAndSort(config *cluster.ContainerConfig, n
 		taskCPU := strconv.FormatInt(cpu,10)
 		taskMemory := strconv.FormatInt(memory,10)
 		go SendInfoHost("http://146.193.41.142:12345/host/updateclass/"+requestClass+"&"+ host.HostIP)
-		SendInfoHost("http://146.193.41.142:12345/host/updateresources/"+host.HostIP+"&"+taskCPU+"&"+taskMemory+"&0")
+		SendInfoHost("http://146.193.41.142:12345/host/updateresources/"+host.HostIP+"&"+taskCPU+"&"+taskMemory)
 		return output, nil, requestClass, requestType, cut
 	}
 
@@ -202,7 +202,7 @@ func (p *EnergyPlacementStrategy) RankAndSort(config *cluster.ContainerConfig, n
 		taskCPU := strconv.FormatInt(config.HostConfig.CPUShares,10)
 		taskMemory := strconv.FormatInt(config.HostConfig.Memory,10)
 		go SendInfoHost("http://146.193.41.142:12345/host/updateclass/"+requestClass+"&"+ host.HostIP)
-		SendInfoHost("http://146.193.41.142:12345/host/updateresources/"+host.HostIP+"&"+taskCPU+"&"+taskMemory+"&0")
+		SendInfoHost("http://146.193.41.142:12345/host/updateresources/"+host.HostIP+"&"+taskCPU+"&"+taskMemory)
 		return output, nil, requestClass, requestType,  0.0
 	}
 	fmt.Println("Not allocable")
